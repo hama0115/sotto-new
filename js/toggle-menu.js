3266056e699cf6e248f2ccb302435804d1216004
hama0115 <oh.akibee1231@gmail.com>
2025-03-03 12:50:25 +0900

    ハンバーガーメニューの挙動を修正。ページ内リンクを押してもメニューが閉じるようにjsをアレンジ

--- js/toggle-menu.js
+++ js/toggle-menu.js
@@ -1,6 +1,8 @@
 window.addEventListener('load', function () {
     var $button = document.querySelector('.toggle-menu-button');
     var $menu = document.querySelector('.site-menu-wrapper');
+    var $links = document.querySelectorAll('.site-menu a');
+
     $button.addEventListener('click', function () {
         if ($menu.classList.contains('is-show')) {
             $menu.classList.remove('is-show');
@@ -8,5 +10,13 @@ window.addEventListener('load', function () {
         else {
             $menu.classList.add('is-show');
         }
+    });
+
+    $links.forEach(function ($link) {
+        $link.addEventListener('click', function () {
+            if ($menu.classList.contains('is-show')) {
+                $menu.classList.remove('is-show');
+            }
+        });
     });
 });
